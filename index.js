c43c8107dd98b19770ec3ab1aeb403ad0669e0c7
Stella Miranda <stella.miranda@unbounce.com>
2020-08-05 15:26:01 -0700

    Copy update

--- index.js
+++ index.js
@@ -14,10 +14,32 @@ slackEvents.on('message', (event) => {
     (async () => {
       const messageBody =
         process.env.GUYS_MESSAGE ||
-        'Please bear in mind that the makeup of this Slack is ' +
-        'very diverse, and some people feel excluded by the ' +
-        'use of the term “guys”. Maybe you could try using ' +
-        '_people_, _team_, _all_, _folks_, _everyone_, or _y\'all_?'
+        "Did you know the term 'guys' would assume everyone you're "+
+        "talking to identifies with the male-gender? You can consider " +
+        "using _y'all_, _crew_, _people_, _friends_, _everyone_ or _team_."
+
+      const infoLink =
+        process.env.GUYS_INFO_LINK ||
+        'https://dev.to/kmelve/the-problem-with-you-guys-51h7'
+
+      await web.chat.postEphemeral({
+        channel: event.channel,
+        user: event.user,
+        text: messageBody + ` (<${infoLink}|What\'s this then?>).`
+      })
+    })()
+  }
+
+  if (
+    Object.prototype.hasOwnProperty.call(event, 'text') &&
+    event.text.toLowerCase().includes('tribe')
+  ) {
+    (async () => {
+      const messageBody =
+        process.env.GUYS_MESSAGE ||
+        "Did you know European colonists used this term synonymously "+
+        "with ‘savage’ or ‘primitive’? At Unbounce, we've transitioned "+
+        "to the term 'team'."
       const infoLink =
         process.env.GUYS_INFO_LINK ||
         'https://dev.to/kmelve/the-problem-with-you-guys-51h7'
